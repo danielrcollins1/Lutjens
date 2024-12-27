544832a6a8467de87c052e2067951597fdafa1d0
Daniel R. Collins ("Delta") <dcollins@superdan.net>
2024-12-27 03:11:02 -0500

    Avoid Faeroe Islands on first turn

--- GermanPlayer.cpp
+++ GermanPlayer.cpp
@@ -401,11 +401,14 @@ void GermanPlayer::getDirection(Ship& ship) {
 	
 	// Breakout bonus move destination
 	if (game->getTurnsElapsed() == 0) {
+		int col = 15 + rollDie(4) - 1;
 		char row = 'A' + rollDie(4) - 1;
 		if (rollDie(100) <= 15) {
 			row = 'E' + rollDie(2) - 1;
 		}
-		int col = 15 + rollDie(4) - 1;
+		if (row == 'F' && col == 15) { // Faeroe
+			row = 'A' + rollDie(5) - 1;
+		}
 		ship.addWaypoint(GridCoordinate(row, col));
 	}
 
